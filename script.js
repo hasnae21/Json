23ac413d0bd246176a25cdb8b5a6d1885053fa0c
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-01-25 17:04:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+let myJson ='Products.json';
+let myJS = JSON.parse(myJson);
+// myJs.name;
+
+
+$(function(){       
+$.ajax({
+    url : 'Products.json',
+    datatype : 'json',
+    cache : false ,
+    success: function(data,status){
+        $.each(data, function(index){
+        $('#div').append('<div>'+ data[index].name +'</div>');
+        })
+        console.log(data);
+        console.log(status);
+    },
+    error: function(xhr, textStatus, err){
+       console.log(xhr);
+        console.log(textStatus);
+        console.log(err);
+    }
+    });
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // $.getJSON("Products.json", function(data){
+        //     console.log(data);
+        
+        //     var content = '';
+        //     for (var x in data){
+            //         content = data[x].name;  
+            //     }
+            //     console.log(content);//avoir tous les noms
+            // $("td").html("content");
+            // // $("div").css("font-family","calibri");
+    // });
