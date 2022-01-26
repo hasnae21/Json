3156b6cd97d99071f280194b8d7f0d824dd88f58
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-01-26 16:53:47 +0100

    lll

--- js/script.js
+++ js/script.js
@@ -1,42 +1,55 @@
-let myJson ='Products.json';
-console,log(myJson);
-let myJS = JSON.parse(myJson);
-// myJs.name;
 
-
-$(function(){       
+$(document).ready(function(){    
 $.ajax({
-    url : 'Products.json',
+    url : "Produits.json",
     datatype : 'json',
     cache : false ,
+
     success: function(data,status){
-        $.each(data, function(index){
-        $('#div').append('<div>'+ data[index].name +'</div>');
-        })
+        for (var x in data) {
+                var tr = `<tr>
+                    <td>${data[x].Un_ID}</td>
+                    <td>${data[x].Une_désignation}</td>
+                    <td>${data[x].Un_prix}</td>
+                    <td>${data[x].Une_catégorie}</td>
+                    <td>${data[x].Une_disponibilité}</td>
+                    <td>${data[x].Un_fournisseur.Raison_Sociale} <br>
+                        ${data[x].Un_fournisseur.Adresse}
+                    </td>
+                </tr>`
+                var table = $("#mytbody")
+                table.append(tr)
+        }
         console.log(data);
         console.log(status);
     },
+
     error: function(xhr, textStatus, err){
        console.log(xhr);
         console.log(textStatus);
         console.log(err);
     }
-    });
-}); 
+    })
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// $('#search').keydown(function(){
+//     $.getJSON("Produits.json",function(data){
+//         var search = $('#search').val();
+//         var regex = new RegExp(search, 'i');
+//         var output;
+//         $.each(data, function(key, val){
+//             if((val.id.search(regex) != -1) || (val.name.search(regex) != -1)){
+//                 output += "<tr>";
+//                 output += "<td id='"+key+"'>" + val.Un_ID + "</td>";
+//                 output += "<td id='"+key+"'>" + val.Une_désignation + "</td>";
+//                 output += "<td id='"+key+"'>" + val.Un_prix + "</td>";
+//                 output += "<td id='"+key+"'>" + val.Une_catégorie + "</td>";
+//                 output += "</tr>";
+//             }
+//         });
+//     $('tbody').html(output)
+//     });
+// });
 
 
 
