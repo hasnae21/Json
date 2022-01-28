05d2154034e14d6baa3e12dbdd9d67bf1fc8b172
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-01-28 17:02:52 +0100

    g

--- js/script.js
+++ js/script.js
@@ -1,79 +1,86 @@
+// $(document).ready(function(){    
+// $.ajax({
+//     url : "Produits.json",
+//     datatype : 'json',
+//     cache : false ,
 
-$(document).ready(function(){    
-$.ajax({
-    url : "Produits.json",
-    datatype : 'json',
-    cache : false ,
+//     success: function(data,status){
+//         for (var x in data) {
+//                 var tr = `<tr>
+//                     <td>${data[x].Année_de_sortie}</td>
+//                     <td>${data[x].désignation}</td>
+//                     <td>${data[x].prix}</td>
+//                     <td>${data[x].fournisseur}</td>
+//                     <td>${data[x].Catégorie}</td>
+//                     <td>${data[x].Carburant}</td>
+//                     <td>${data[x].Boite_de_vitesses}</td>
+//                     <td>${data[x].consomation.ville}</td>
+//                     <td>${data[x].consomation.route}</td>
+//                     <td>${data[x].consomation.mixte}</td>
+//                     <td>
+//                         <ul>
+//                         <li>${data[x].securite[0]}</li> 
+//                         <li>${data[x].securite[1]}</li> 
+//                         <li>${data[x].securite[2]}</li> 
+//                         <li>${data[x].securite[3]}</li> 
+//                         <li>${data[x].securite[4]}</li>
+//                         </ul>
+//                     </td>
+//                 </tr>`
+//                 var table = $("#mytbody")
+//                 table.append(tr)
+//         }
+//         console.log(data);
+//         console.log(status);
+//     },
 
-    success: function(data,status){
-        for (var x in data) {
-                var tr = `<tr>
-                    <td>${data[x].Un_ID}</td>
-                    <td>${data[x].Une_désignation}</td>
-                    <td>${data[x].Un_prix}</td>
-                    <td>${data[x].Une_catégorie}</td>
-                    <td>${data[x].Une_disponibilité}</td>
-                    <td>${data[x].Un_fournisseur.Raison_Sociale} <br>
-                        ${data[x].Un_fournisseur.Adresse}
-                    </td>
-                </tr>`
-                var table = $("#mytbody")
-                table.append(tr)
-        }
-        console.log(data);
-        console.log(status);
-    },
+//     error: function(xhr, textStatus, err){
+//        console.log(xhr);
+//         console.log(textStatus);
+//         console.log(err);
+//     }
+//     })
 
-    error: function(xhr, textStatus, err){
-       console.log(xhr);
-        console.log(textStatus);
-        console.log(err);
-    }
-    })
-});
+// });
 
-// $('#search').keydown(function(){
-//     $.getJSON("Produits.json",function(data){
-//         var search = $('#search').val();
-//         var regex = new RegExp(search, 'i');
-//         var output;
-//         $.each(data, function(key, val){
-//             if((val.id.search(regex) != -1) || (val.name.search(regex) != -1)){
-//                 output += "<tr>";
-//                 output += "<td id='"+key+"'>" + val.Un_ID + "</td>";
-//                 output += "<td id='"+key+"'>" + val.Une_désignation + "</td>";
-//                 output += "<td id='"+key+"'>" + val.Un_prix + "</td>";
-//                 output += "<td id='"+key+"'>" + val.Une_catégorie + "</td>";
-//                 output += "</tr>";
-//             }
+
+// $(document).ready(function(){
+//     $("th").each(function(column){
+//         $(this).hover(function(){
+//             $(this).data("type", $(this).attr("class"));
 //         });
-//     $('tbody').html(output)
+//         $(this).click(function(){
+//             var type = $(this).data("type");
+//             var records = $("table").find("tbody > tr");
+//             records.sort(function(a, b){
+//                 var value1 = $(a).children("td").eq(column).text();
+//                 var value2 = $(b).children("td").eq(column).text();
+//                 if(type == "num")
+//                 {
+//                     value2 *= 1;
+//                     value2 *= 1;
+//                 }
+//                 return (value1<value2) ? -1 : (value1>value2 ? 1 : 0)
+//             });
+//             $.each(records, function(index, row){
+//                 $("tbody").append(row);
+//             })    
+//         });
 //     });
 // });
 
 
+// $(document).ready(function(){
+// $("#search").on("keyup",function(){
+//     var value = $(this).val();
+// $("#mytbody tr").filter(function(){
+//     $(this).toggle($(this).text().indexOf(value) > -1);
+// });
+// });
+// });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    // $.getJSON("Products.json", function(data){
-        //     console.log(data);
-        
-        //     var content = '';
-        //     for (var x in data){
-            //         content = data[x].name;  
-            //     }
-            //     console.log(content);//avoir tous les noms
-            // $("td").html("content");
-            // // $("div").css("font-family","calibri");
-    // });
+function tab(a,b){
+    return a-b;
+}
+console.log(tab(21.7 , 9.6));
